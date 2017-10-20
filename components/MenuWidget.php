a086266c175aa36952ce13ef7f2d7942d11fd7e5
dest2r4 <alkhonko@gmail.com>
2017-10-20 11:16:58 +0300

    add simple widget

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -6,7 +6,27 @@
  * Time: 20:56
  */
 
-class MenuWidget
-{
+namespace app\components;
+use yii\base\Widget;
 
+
+class MenuWidget extends Widget
+{
+    public $tpl;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->tpl===null)
+        {
+            $this->tpl='menu';
+        }
+        $this->tpl.='.php';
+
+    }
+
+    public function run()
+    {
+        return $this->tpl;
+    }
 }
